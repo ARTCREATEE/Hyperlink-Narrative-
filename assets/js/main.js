d6e0c391006b8e6e39640e62ed1ffd8ce0a6a167
ARTCREATEE <Arenewearing@gmail.com>
2026-03-05 22:16:00 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,5 @@
 // main.js
 
-const customCursor = document.getElementById('custom-cursor');
-const hoverContainer = document.querySelector('.hover-container');
 
-const updateCursorPosition = (event) => {
-  customCursor.style.top = `${event.clientY}px`;
-  customCursor.style.left = `${event.clientX}px`;
-}
-
-window.addEventListener('mousemove', (event) => {
-  updateCursorPosition(event)
 
 console.log("Hello, World!");
